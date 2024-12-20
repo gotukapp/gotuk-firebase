7bbe78f2ff5b708a8892fb89345c9e303252cbd3
jpenas <jppenas@gmail.com>
2024-12-20 10:46:57 +0000

    tour start notifications

--- functions/index.js
+++ functions/index.js
@@ -1,39 +1,20 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+const {
+  onDocumentCreated,
+} = require("firebase-functions/v2/firestore");
+const {onSchedule} = require("firebase-functions/v2/scheduler");
+
 
 admin.initializeApp();
 
 // Example HTTPS function to send a notification
 exports.sendNotification = functions.https.onRequest(async (req, res) => {
   try {
-    // Target device token or topic
     const {token, topic, title, body, data} = req.body;
 
+    const message = getMessage(title, body, data, token, topic);
 
-    // Construct the message payload
-    const message = {
-      notification: {
-        title: title || "Default Title",
-        body: body || "Default Body",
-      },
-      android: {
-        notification: {
-          sound: "default",
-        },
-      },
-      apns: {
-        payload: {
-          aps: {
-            sound: "default",
-          },
-        },
-      },
-      data: data || {}, // Custom key-value pairs
-      token: token, // Target a specific device
-      topic: topic, // Or target a topic
-    };
-
-    // Send the message
     const response = await admin.messaging().send(message);
     // eslint-disable-next-line max-len
     res.status(200).send({message: "Notification sent successfully", response});
@@ -44,13 +25,86 @@ exports.sendNotification = functions.https.onRequest(async (req, res) => {
   }
 });
 
-const {
-  onDocumentCreated,
-} = require("firebase-functions/v2/firestore");
+// eslint-disable-next-line max-len
+exports.startTripNotification = onSchedule("*/30 * * * *", async (event) => {
+  console.log("Scheduled function executed at:", new Date().toISOString());
+  try {
+    const db = admin.firestore();
+    const query = db.collection("trips")
+        .where("status", "==", "booked")
+        .where("date", "<=", new Date(new Date().getTime() + (30 * 60 * 1000)))
+        // eslint-disable-next-line max-len
+        .where("date", ">=", new Date(new Date().getTime() - (2 * 60 * 60 * 1000)));
+
+    const trips = await query.get();
+
+    for (const trip of trips.docs) {
+      try {
+        const tour = await trip.get("tourId").get();
+        const tourName = tour.get("name");
+        const guide = await trip.get("guideRef").get();
+        const client = await trip.get("clientRef").get();
+        const tripDate = trip.get("date").toDate();
+
+        const tripTime = tripDate.toLocaleTimeString("pt-PT",
+            {hour: "2-digit", minute: "2-digit"});
+
+        let title = new Date().getTime() < tripDate.getTime() ?
+            "Próximo tour começa em breve!" :
+            "Tem um tour por iniciar!";
+
+        let body = tourName +
+            "\n" + new Date().getTime() < tripDate.getTime() ?
+            "Prepare-se: você tem um tour às " + tripTime + "!" :
+            "Atenção: este tour já devia ter iniciado às " + tripTime + "!";
+
+        if (guide.exists()) {
+          const documentData = guide.data();
+          const hasField = "firebaseToken" in documentData;
+          if (hasField) {
+            // eslint-disable-next-line max-len
+            const message = getMessage(title, body, null, guide.get("firebaseToken"));
+            const response = await admin.messaging().send(message);
+            console.info("Notification sent successfully", response);
+          }
+        }
+
+        title = new Date().getTime() < tripDate.getTime() ?
+            "Seu tour começa em breve!" :
+            "Tem um tour por iniciar!";
+
+        body = tourName +
+            "\n" + new Date().getTime() < tripDate.getTime() ?
+            "Não perca: seu tour começa às " + tripTime + "!" :
+            "Urgente: seu tour deveria ter iniciado às " + tripTime + "!";
+
+        if (client.exists()) {
+          const documentData = client.data();
+          const hasField = "firebaseToken" in documentData;
+          if (hasField) {
+            // eslint-disable-next-line max-len
+            const message = getMessage(title, body, null, client.get("firebaseToken"));
+            const response = await admin.messaging().send(message);
+            console.info("Notification sent successfully", response);
+          }
+        }
+      } catch (error) {
+        console.error("Error sending notification", trip, error);
+      }
+    }
+  } catch (error) {
+    console.error("Error executing scheduled task:", error);
+  }
+
+  return null;
+});
 
 exports.newTripNotification = onDocumentCreated("trips/{docId}",
     async ({data}) => {
       try {
+        if (data.get("status") !== "pending") {
+          return;
+        }
         const db = admin.firestore();
         const tour = await data.get("tourId").get();
         const tourName = tour.get("name");
@@ -110,29 +164,14 @@ exports.newTripNotification = onDocumentCreated("trips/{docId}",
 
         for (const guide of filteredGuides) {
           try {
-            const message = {
-              notification: {
-                title: "Novo Go Now",
-                body: tripDate.toLocaleString("pt-PT") + " - " + tourName +
-                    "\nEntre na App para aceitar a viagem.",
-              },
-              android: {
-                notification: {
-                  sound: "default",
-                },
-              },
-              apns: {
-                payload: {
-                  aps: {
-                    sound: "default",
-                  },
-                },
-              },
-              data: {}, // Custom key-value pairs
-              // eslint-disable-next-line max-len
-              token: guide.get("firebaseToken"),
-              topic: null,
-            };
+            const body = tripDate.toLocaleString("pt-PT") + " - " + tourName +
+                "\nEntre na App para aceitar a viagem.";
+
+            const message = getMessage("Novo Go Now",
+                body,
+                null,
+                guide.get("firebaseToken"));
+
             // Send the message
             const response = await admin.messaging().send(message);
 
@@ -145,3 +184,28 @@ exports.newTripNotification = onDocumentCreated("trips/{docId}",
         console.error("Error sending notification:", error);
       }
     });
+
+// eslint-disable-next-line require-jsdoc
+function getMessage(title, body, data, token) {
+  return {
+    notification: {
+      title: title || "Default Title",
+      body: body || "Default Body",
+    },
+    android: {
+      notification: {
+        sound: "default",
+      },
+    },
+    apns: {
+      payload: {
+        aps: {
+          sound: "default",
+        },
+      },
+    },
+    data: data || {},
+    token: token,
+    topic: null,
+  };
+}
